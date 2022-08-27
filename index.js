11cf21977de2312703fbb3a494e4d1f433a76930
bedpoopers <106645269+bedpoopers@users.noreply.github.com>
2022-08-26 23:10:03 -0700

    Implemented NUMPHOTOS constant

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
   function init() {
     let photos = document.querySelectorAll('.photo');
     for (let i = 0; i < photos.length; i++) {
-      let x = Math.floor((Math.random() * 352)+1);;
+      let x = Math.floor((Math.random() * NUMPHOTOS)+1);;
       photos[i].src = "img/img (" + x + ").jpg";
       photos[i].addEventListener('mouseover', changePhoto);
       photos[i].addEventListener('click', changePhoto);
@@ -15,7 +15,7 @@
   }
 
   function changePhoto(){
-    let x = Math.floor((Math.random() * 352)+1);;
+    let x = Math.floor((Math.random() * NUMPHOTOS)+1);;
     this.src = "img/img (" + x + ").jpg";
   }
 })();
